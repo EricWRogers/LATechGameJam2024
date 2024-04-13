4ae7b6a42caa92987b67b1ad77b129b97cf50294
Xander Adams <xanderadams555@gmail.com>
2024-04-13 14:19:16 -0500

    Pickup USB

---
+++ Assets/Scripts/Upgrade Scripts/PickUpUpgrade.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpUpgrade : MonoBehaviour
+{
+    public UpgradeSystem upgradeSystem;
+    public UpgradeHolder upgradeHolder;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            upgradeSystem.AddUpgrade(upgradeHolder.upgrade);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
