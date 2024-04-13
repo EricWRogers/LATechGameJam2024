045a6f89e79434c91c21986f93928ea12a1db03f
Garrett Shipp <garwad9@gmail.com>
2024-04-13 01:50:23 -0500

    enemy patrol done

---
+++ Assets/Scripts/AI Scripts/ChaseState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperPupSystems.Helper;
+using SuperPupSystems.StateMachine;
+
+[System.Serializable]
+public class ChaseState : SimpleState
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
