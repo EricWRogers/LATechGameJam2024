7ed119af0374f9dbab9a4f66d721d370b07abba2
Garrett Shipp <garwad9@gmail.com>
2024-04-13 17:07:12 -0500

    Finished ranged prefab

--- Assets/Scripts/AI Scripts/AttackState.cs
+++ Assets/Scripts/AI Scripts/AttackState.cs
@@ -4,43 +4,59 @@ using UnityEngine;
 using SuperPupSystems.Helper;
 using SuperPupSystems.StateMachine;
 using UnityEngine.AI;
-using Unity.VisualScripting;
 using UnityEngine.Events;
-using System.Threading;
 using static UnityEngine.GraphicsBuffer;
 
 [System.Serializable]
 public class AttackState : SimpleState
 {
-    public UnityEvent attack;    
+    public Timer time;
+    public UnityEvent attack;  
+    public UnityEvent stopAttacking;
     public NavMeshAgent agent;
-    public float attackRange;
     private bool playerInRange;
     private bool isAttacking;
+
 
 
     public override void OnStart()
     {
         Debug.Log("Attack State");
         base.OnStart();
+        time.StartTimer(2, true);
         if (attack == null)
             attack = new UnityEvent();
-        ((RangedEnemyStateMachine)stateMachine).transform.LookAt(((RangedEnemyStateMachine)stateMachine).target);
-        agent.SetDestination(((RangedEnemyStateMachine)stateMachine).target.position);
+        agent.SetDestination(((RangedEnemyStateMachine)stateMachine).transform.position);
 
     }
 
     public override void UpdateState(float dt)
     {
-        playerInRange = Physics.CheckSphere(((RangedEnemyStateMachine)stateMachine).transform.position, attackRange, LayerMask.GetMask("Player"));
-        if(playerInRange && !isAttacking)
+        ((RangedEnemyStateMachine)stateMachine).transform.LookAt(((RangedEnemyStateMachine)stateMachine).target);
+        Debug.Log("" + time.timeLeft);
+        if (((RangedEnemyStateMachine)stateMachine).LOS == true && !isAttacking)
         {
-            agent.SetDestination(((RangedEnemyStateMachine)stateMachine).transform.position);
+            Debug.Log("Attaking");
             isAttacking = true;
             attack.Invoke();
+            
         }
-        
+        if (((RangedEnemyStateMachine)stateMachine).LOS == false)
+        {
+            time.autoRestart = false;
+            if(time.timeLeft <= 0)
+            {
+                isAttacking = false;
+                stopAttacking.Invoke();
+                stateMachine.ChangeState(nameof(MoveInRangeState));
+            }
+            
+        }
+
+
+
     }
+    
     public override void OnExit()
     {
         base.OnExit();
