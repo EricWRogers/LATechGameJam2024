3cf7f48dd277b9cde53aa1d4c862cf2654ea50ab
Garrett Shipp <garwad9@gmail.com>
2024-04-14 12:38:22 -0500

    Boom on death maybe??

--- Assets/Scripts/AI Scripts/ChargeState.cs
+++ Assets/Scripts/AI Scripts/ChargeState.cs
@@ -13,10 +13,7 @@ public class ChargeState : SimpleState
     public Timer time;
     public NavMeshAgent agent;
     public float power;
-    public float radius;
-    public int dmg;
-    Health health;
-    Rigidbody rb;
+    
 
 
     public override void OnStart()
@@ -24,7 +21,7 @@ public class ChargeState : SimpleState
         Debug.Log("Charge State");
         base.OnStart(); 
         agent.SetDestination(((ChargeStatMachine)stateMachine).transform.position);
-        Explode();
+        ((ChargeStatMachine)stateMachine).Explode();
     }
 
     public override void UpdateState(float _dt)
@@ -32,26 +29,5 @@ public class ChargeState : SimpleState
             base.UpdateState(_dt);
         
     }
-    public void Explode()
-    {
-        Collider[] colliders = Physics.OverlapSphere(((ChargeStatMachine)stateMachine).transform.position, radius);
-
-        foreach(Collider hit in colliders)
-        {
-            Debug.Log("BOOM");
-            //hit.GetComponent<Health>()?.Damage(dmg);
-            if (hit.transform.parent)
-            {
-                hit.transform.parent.GetComponentInChildren<Health>()?.Damage(dmg);
-                //hit.transform.parent.GetComponentInChildren<Rigidbody>()?.isKinematic;
-                //hit.transform.parent.GetComponentInChildren<Rigidbody>()?.AddExplosionForce(power, ((ChargeStatMachine)stateMachine).target.position, radius, 0);
-            }
-            else
-            {
-                hit.GetComponentInChildren<Health>()?.Damage(dmg);
-                //hit.transform.GetComponentInChildren<Rigidbody>()?.AddExplosionForce(power, ((ChargeStatMachine)stateMachine).target.position, radius, 0);
-            }
-
-        }
-    }
+    
 }
