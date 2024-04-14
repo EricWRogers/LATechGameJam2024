601e12a9b2e8c84180e4eb59d9b9064257e58863
Logan Hobson <Loganhobson31@gmail.com>
2024-04-14 09:55:34 -0500

    Anim fixes

--- Assets/Scripts/WheatlyHurt.cs
+++ Assets/Scripts/WheatlyHurt.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class WheatlyHurt : MonoBehaviour
 {
     int rand;
+    
     public void PlayHurt()
     {
         rand = Random.Range(0, 2);
         switch (rand)
         {
+            case 0:
 
         }
     }
