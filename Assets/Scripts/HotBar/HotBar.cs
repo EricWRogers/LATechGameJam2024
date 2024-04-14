7cdd798734e3cccd8d0b52e81ebe6f33bd9fb2b4
Aiden Thacker <aidenthacker86@gmail.com>
2024-04-14 12:24:48 -0500

    Trying to finish up the HotBar

---
+++ Assets/Scripts/HotBar/HotBar.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HotBar : MonoBehaviour
+{
+    public List<Image> imageIcons;
+    public UpgradeSystem playerUpgradeSystem;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerUpgradeSystem = GameObject.FindWithTag("Player").GetComponent<UpgradeSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerUpgradeSystem.upgrades.Count > 0 && imageIcons.Count < 5)
+        {
+            UpdateHotBar();
+        }
+    }
+
+    void UpdateHotBar()
+    {
+        for (int i = 0; i < imageIcons.Count; i++)
+        {
+            if (i < playerUpgradeSystem.upgrades.Count-1)
+            {
+                Debug.Log("Switch Icon Image with Upgrade Sprite");
+                imageIcons[i].sprite = playerUpgradeSystem.upgrades[i].upgradeSprite;
+            }
+            else
+            {
+                imageIcons[i].sprite = null;
+            }
+        }
+    }
+}
