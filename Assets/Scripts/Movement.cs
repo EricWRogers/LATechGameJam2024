050f478a88da281c9d4156919b697b31009247eb
Xander Adams <xanderadams555@gmail.com>
2024-04-14 12:38:44 -0500

    dash pending

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -26,7 +26,7 @@ public class Movement : MonoBehaviour
     public GameObject groundCheck;
     public UpgradeSystem upgradeSystem;
     public float mag;
-    public bool antiSlip;
+    public bool antiSlip = true;
     public float mouseSensitivity = 2.0f;
     public float upDownRange = 60.0f;
     float verticalRotation = 0;
@@ -125,14 +125,14 @@ public class Movement : MonoBehaviour
 
             return (velocityChange);
         }
-        else //if(/*antiSlip*/)
+        else if(antiSlip)
         {
             return new Vector3();
         }
-        // else
-        // {
-        //     return velocity;
-        // }
+        else
+        {
+            return velocity;
+        }
 
 
     }
